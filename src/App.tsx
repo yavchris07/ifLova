085bd6d098c40c5afc9aec91b1412f3b9ee58746
yavchris07 <wilondjayav27@gmail.com>
2024-09-16 11:53:14 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+ import './App.css';
+ import Routing from './routing';
+
+function App() {
+  return (
+    <div className="App">
+      <p> If Lova en cours de Dev !</p>
+      <Routing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routing.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//Type or a list of our pages
+type Pages = {}
+
+//name, path, element
+
+export default function Routing() {
+  return (
+    <div>
+        Routing
+    </div>
+  )
+}
