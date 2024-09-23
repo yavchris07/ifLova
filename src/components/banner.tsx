adfd6cd426a2fe3b43618fa4489965a9fdb23243
yavchris07 <wilondjayav27@gmail.com>
2024-09-23 10:53:18 +0200

    banner review

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -4,7 +4,10 @@ import '../styles/app.scss'
 export default function Banner() {
   return (
     <div className='banner'>
-      
+      <div className='cover'>
+        <h3>IFlova TV1</h3>
+        <p>votre source principale pour les dernières nouvelles de la République Démocratique du Congo et d'autres pays.</p>
+      </div>
     </div>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import Banner from '../components/banner'
+import Banner from '../components/banner';
+import '../styles/home.scss';
 
 function Home() {
   return (
     <>
     <Banner />
+    <div className='home'>
       <p>
-      votre source principale pour les dernières nouvelles de la République Démocratique du Congo et d'autres pays. Nous vous tenons informés des événements importants, des développements sécuritaires et des mises à jour essentielles pour vous aider à rester au fait de l'actualité régionale et internationale.
-      Mais ce n'est pas tout - chez IFlova TV1, nous savons que le divertissement est tout aussi crucial. C'est pourquoi nous vous proposons une gamme diversifiée de contenus divertissants, des interviews exclusives aux critiques de films, en passant par les dernières tendances musicale.
+        Nous vous tenons informés des événements importants, des développements sécuritaires et des mises à jour essentielles pour vous aider à rester au fait de l'actualité régionale et internationale.
+        Mais ce n'est pas tout - chez IFlova TV1, nous savons que le divertissement est tout aussi crucial. C'est pourquoi nous vous proposons une gamme diversifiée de contenus divertissants, des interviews exclusives aux critiques de films, en passant par les dernières tendances musicale.
       </p>
+
+      <div className='list-card'>
+
+      </div>
+
+       
+
+    </div>
+     
     </>
   )
 }
