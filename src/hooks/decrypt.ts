dabc3255672d119b2fcfc70306f691a7c956d273
yavchris07 <wilondjayav27@gmail.com>
2024-12-04 05:17:37 +0200

    adding encrypt and decrypt hook

---
+++ src/hooks/decrypt.ts
@@ -0,0 +1,16 @@
+// import CryptoJS from "crypto-js";
+
+export const SECRET_KEY = "EncryptSkool";
+
+// export function encryptId(id: string | number): string {
+//     const idStr = id.toString();
+//     // const ciphertext = CryptoJS.AES.encrypt(idStr, SECRET_KEY).toString();
+//     // return encodeURIComponent(ciphertext);
+//   }
+  
+//   // Fonction pour déchiffrer l'ID
+//   export function decryptId(encryptedId: string): string {
+//     // const bytes = CryptoJS.AES.decrypt(decodeURIComponent(encryptedId), SECRET_KEY);
+//     // return bytes.toString(CryptoJS.enc.Utf8);
+//   }
+  
--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import art from '../types/items';
 
 export default function Article() {
+  const { id } = useParams<{ id: string }>();
+  // const { encryptedId } = useParams<{ encryptedId: string }>();
+  // const {skool} = useFetchSkools()
+  let article;
+
+  if (id) {
+    // const id = decryptId(encryptedId);
+    article = art.find(a => a.id === parseInt(id));
+  }
+
+  if (!article) {
+    return <div className='not-school-found'>Chargement en cours, veuillez patienter !</div>
+  }
   return (
-    <div>
-      
+    <div className='article'>
+      <p>{article.summary}</p>
     </div>
   )
 }
